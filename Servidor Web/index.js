eda44dd1645de2200ca2c5b8ba905ef741760d33
Edgar Daniel Rojas Vazquez <edgardanielprog@gmail.com>
2019-02-19 10:55:43 -0600

    Added initial files

---
+++ Servidor Web/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var users = require('./users/users.js');
+var fs = require('fs');
+var multer = require('multer');
+var uploadFolder = multer({dest:'uploads/'});
+
+var app = express();
+
+app.use( cookieParser() );
+app.use( express.static('./public_html') );
+app.use('/users' , users);
+
+app.use('/*' , (req,res,next)=>{
+    console.log(req.cookies); 
+    next();
+});
+
+app.post('/uploadFile' , uploadFolder.single('File1') , (req,res, next)=>{
+    
+    fs.renameSync(req.file.path, req.file.destination + req.file.originalname);
+    
+    console.log(req.file);
+    res.end('done');
+} );
+
+app.get('/setCookie' , (req,res)=>{
+    res.cookie('myCookie','data');
+    res.end("cookie set");
+});
+
+app.get('/ping' , (req,res) => {
+    res.set('Content-type','text/html');
+    res.send("<div>Pong</div>");
+    res.end();
+} );
+
+app.get('/' , (req,res)=>{
+    res.redirect('/index.html');
+    //console.log("get 1");
+    //res.send("Hello!");
+});
+
+app.listen(3030);
